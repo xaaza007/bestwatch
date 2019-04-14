d0e3b5a7cd5131c928921c4f0941051b75f19738
Jakub Wiśniewski <jakwis93@gmail.com>
2019-04-14 12:49:27 +0200

    Form.html added and mapped post/get

--- src/main/java/pl/sda/bestwatch/BestwatchMvcController.java
+++ src/main/java/pl/sda/bestwatch/BestwatchMvcController.java
@@ -1,10 +1,13 @@
 package pl.sda.bestwatch;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/app/bestwatch/suggestions")
 @Controller
 public class BestwatchMvcController {
 
@@ -15,10 +18,21 @@ public class BestwatchMvcController {
         this.suggestionRepository = suggestionRepository;
     }
 
-    @GetMapping("/app/bestwatch/suggestions")
+    @GetMapping
     public String allSuggestionsView(Model indexViewModel) {
         indexViewModel.addAttribute("allSuggestions", suggestionRepository.findAll());
 
         return "index";
+    }
+
+    @PostMapping
+    public String addSuggestion(Suggestion suggestion) {
+        suggestionRepository.save(suggestion);
+        return "redirect:/app/bestwatch/suggestions";
+    }
+
+    @GetMapping("/form")
+    public String form() {
+        return "form";
     }
 }
